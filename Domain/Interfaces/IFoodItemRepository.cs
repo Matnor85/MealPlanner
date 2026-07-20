475d82cc4c9f067ee0dfad686de7e4527bda8688
Mathias <Mathias.norrbacken@gmail.com>
2026-07-20 23:49:42 +0200

    .

---
+++ Domain/Interfaces/IFoodItemRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public interface IFoodItemRepository
+{
+    Task<IEnumerable<FoodItem>> GetAllAsync();
+    Task<FoodItem?> GetByIdAsync(Guid id);
+    Task AddAsync(FoodItem food);
+    Task DeleteAsync(Guid id);
+}
